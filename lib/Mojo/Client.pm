fd9e418d93e6b5ebb71a06d2c1674438a544ada0
Acajou <acajou@gmail.com>
2009-08-16 08:06:56 -0400

    When peer closes socket, $s->peerport and $s->sockport disappear, causing bad mojo in _socket_name.

--- lib/Mojo/Client.pm
+++ lib/Mojo/Client.pm
@@ -452,9 +452,7 @@ sub _socket_name {
     # Generate
     return
         unpack('H*', $s->sockaddr)
-      . $s->sockport
-      . unpack('H*', $s->peeraddr)
-      . $s->peerport;
+      . $s->sockport;
 }
 
 1;
